856f753fc51f43e8afa8c4c9cab368a189995902
Konstantin Stepanov <k.stepanov98@yandex.ru>
2020-10-11 20:47:54 +0300

    initial project structure

---
+++ src/main/java/org/suai/crypto/Main.java
@@ -0,0 +1,7 @@
+package org.suai.crypto;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/suai/crypto/lamport/LamportSignature.java
@@ -0,0 +1,20 @@
+package org.suai.crypto.lamport;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class LamportSignature {
+
+    public static KeyPair generateKeyPair() {
+        throw new UnsupportedOperationException("coming soon");
+    }
+
+    public static byte[] sign(byte[] message, PrivateKey privateKey) {
+        throw new UnsupportedOperationException("coming soon");
+    }
+
+    public static boolean verify(byte[] message, byte[] signature, PublicKey publicKey) {
+        throw new UnsupportedOperationException("coming soon");
+    }
+}
